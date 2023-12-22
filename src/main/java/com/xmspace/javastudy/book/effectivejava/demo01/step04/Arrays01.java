e13ac28a6036deae0c94822e9f9d2ef9b2a4d027
JasonFang <whatdossuperman@gmail.com>
2023-12-22 11:15:35 +0800

    feat,小方：通过私有构造器强化不可实例化的能力

---
+++ src/main/java/com/xmspace/javastudy/book/effectivejava/demo01/step04/Arrays01.java
@@ -0,0 +1,7 @@
+package com.xmspace.javastudy.book.effectivejava.demo01.step04;
+
+public class Arrays01 {
+    private static final int MIN_ARRAY_SORT_GRAN = 1 << 13;
+    // Suppresses default constructor, ensuring non-instantiability.
+    private Arrays01() {}
+}
